277e9bbabb830d7efa7246ee3e5ad95be82630d2
le-Screwball <76546204+le-Screwball@users.noreply.github.com>
2021-09-19 15:27:14 +0530

    Fixed Landing Page

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -8,11 +8,21 @@ import ContinueCard from '../Components/ContinueCard';
 export default function Landing() {
   return (
     <React.Fragment>
-      <Container>
-        <HotlineCard />
-        <CovidResCard />
-        <ContinueCard />
-      </Container>
+      <div className="flexbox-container">
+
+        <div className="flexbox-item Hotlines-Card">
+          <HotlineCard />
+        </div>
+
+        <div className="flexbox-item CovidRes-Card">
+          <CovidResCard />
+        </div>
+
+        <div className="flexbox-item Continue-Card">
+          <ContinueCard />
+        </div>
+
+      </div>
     </React.Fragment>
   );
 }
